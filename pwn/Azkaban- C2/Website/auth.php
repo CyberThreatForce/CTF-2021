ce748ce97fa5b06bb753a093ccf92e788dc30caf
PepeTh3Fr0g <81581448+PepeTh3Fr0g@users.noreply.github.com>
2021-05-18 01:02:08 +0200

    Ajout site Azkaban
    Ajout du site web dans Azkaban c2, pour la post exploit

---
+++ pwn/Azkaban- C2/Website/auth.php
@@ -0,0 +1,15 @@
+<?php 
+$a =  $_POST['email_user']; 
+$b = $_POST['password']; 
+
+if($a == "serguei@secret.com" AND $b == "kuBtV9T}a]YP}")
+{
+	echo "correct password";
+}
+
+else
+{
+	echo "error";
+}
+
+?>
