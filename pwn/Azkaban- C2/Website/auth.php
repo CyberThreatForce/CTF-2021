3de1792573676cad783391fc9b0a11f9d987e965
PepeTh3Fr0g <81581448+PepeTh3Fr0g@users.noreply.github.com>
2021-05-18 01:15:55 +0200

    fin du chall azkaban
    FIN DU CHAL
    Just besoin de host le onion

--- pwn/Azkaban- C2/Website/auth.php
+++ pwn/Azkaban- C2/Website/auth.php
@@ -5,6 +5,8 @@ $b = $_POST['password'];
 if($a == "serguei@secret.com" AND $b == "kuBtV9T}a]YP}")
 {
 	echo "correct password";
+	echo "<!-- Is pretty ctf  for the end d7 33 36 e6 43 e6 33 47 e6 43 d6 f5 e6 13 f5 13 c6 c6 43 57 47 36 43 b7 64 45 25 54 24 95 34 -->";
+
 }
 
 else
