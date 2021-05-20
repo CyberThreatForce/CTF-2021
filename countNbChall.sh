2cddbaac79e4ce52a50232946d8780f257bed8df
requin <requin_citron@protonmail.com>
2021-05-20 17:45:51 +0200

    fix fail

--- countNbChall.sh
+++ countNbChall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #script de shlag
 
-echo "Nb challs: $(find ./ | grep -v '.git'| cut -d '/' -f 3 | uniq  | grep -E '[a-A]' | wc -l)"
+echo "Nb challs: $(find ./ | grep -v '.git'| cut -d '/' -f 3 | uniq  | grep -E '[a-z]|[A-Z]' | wc -l)"
