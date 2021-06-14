95ada178c62dcc5381d54701193d252f0fdb55cf
PepeTh3Fr0g <81581448+PepeTh3Fr0g@users.noreply.github.com>
2021-06-15 01:36:29 +0200

    AJOUT DUN PROGRAMME POUR LANCER SOUS UN AUTRE USEE
    BON JAI PAS TESTER MAIS NORMALEMENT CA MARCHE

---
+++ Programmes_Lab/RunWithDifferentUserMaisBesoinDeLobfu/RunWithDifferentUserMaisBesoinDeLobfu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunWithDifferentUserMaisBesoinDeLobfu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            System.Security.SecureString ssPwd = new System.Security.SecureString();
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Domain = "evilbank.local";
+            proc.StartInfo.UserName = "e.ivazov";
+            string password = "Da845d-d$M8a";
+            for (int x = 0; x < password.Length; x++)
+            {
+                ssPwd.AppendChar(password[x]);
+            }
+            password = "";
+            proc.StartInfo.Password = ssPwd;
+            proc.Start();
+        }
+    }
+}
