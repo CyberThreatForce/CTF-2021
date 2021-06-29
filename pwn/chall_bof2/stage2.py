31a27efe04df368dac66a35d335a7bac354cedda
requin <requin_citron@protonmail.com>
2021-06-29 13:59:18 +0200

    kleman fait des betise dans le train

--- pwn/chall_bof2/stage2.py
+++ pwn/chall_bof2/stage2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
 
-value = os.popen("md5sum /home/ctf-cracked/flag.txt | cut -d ' ' -f 1")
+value = os.popen("md5sum /home/ctf_cracked/flag.txt | cut -d ' ' -f 1")
 if value == "8b098e9d5692641375f8da6d399edf98":
     print("All is clear")
 else:
